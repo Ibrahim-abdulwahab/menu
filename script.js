74951a268413d37a232b4cbbf04539b7081f5104
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-29 17:08:50 +0300

    more efficent javascript

--- script.js
+++ script.js
@@ -4,87 +4,59 @@ const alllunch=document.getElementById("alllunch");
 const allshakes=document.getElementById("allshakes");
 const alldinner=document.getElementById("alldinner");
 
-const breakfast3=document.getElementById("breakfast3");
-const lunch1=document.getElementById("lunch1");
-const shake3=document.getElementById("shake3");
-const breakfast1=document.getElementById("breakfast1");
-const lunch2=document.getElementById("lunch2");
-const shake1=document.getElementById("shake1");
-const breakfast2=document.getElementById("breakfast2");
-const lunch3=document.getElementById("lunch3");
-const shake2=document.getElementById("shake2");
-const dinner=document.getElementById("dinner");
+const breakfasts=document.getElementsByClassName("breakfast");
+const lunches=document.getElementsByClassName("lunch");
+const shakes=document.getElementsByClassName("shake");
+const dinners=document.getElementsByClassName("dinner");
+const allitems=document.getElementsByClassName("meal");
 
 const show=()=>{
-    breakfast3.className="meal";
-    lunch1.className="meal";
-    shake3.className="meal";
-    breakfast1.className="meal";
-    lunch2.className="meal";
-    shake1.className="meal";
-    breakfast2.className="meal";
-    lunch3.className="meal";
-    shake2.className="meal";
-    dinner.className="meal";
+    for (let i=0; i<allitems.length;i++){
+        allitems[i].setAttribute("visibility", "true")
+    }
 }
 
 all.addEventListener("click",show)
 
 const showBreakfast=()=>{
-    lunch1.className="none";
-    shake3.className="none";
-    lunch2.className="none";
-    shake1.className="none";
-    lunch3.className="none";
-    shake2.className="none";
-    dinner.className="none";
-    breakfast1.className="meal";
-    breakfast2.className="meal";
-    breakfast3.className="meal";
+    for (let i=0; i<allitems.length;i++){
+        allitems[i].setAttribute("visibility", "false")
+    }
+    for (let i=0; i<breakfasts.length;i++){
+        breakfasts[i].setAttribute("visibility", "true")
+    }
 }
 
 allbreakfast.addEventListener("click",showBreakfast)
 
 const showLunch = ()=>{
-    breakfast3.className="none";
-    shake3.className="none";
-    breakfast1.className="none";
-    shake1.className="none";
-    breakfast2.className="none";
-    shake2.className="none";
-    dinner.className="none";
-    lunch1.className="meal";
-    lunch2.className="meal";
-    lunch3.className="meal";
+    for (let i=0; i<allitems.length;i++){
+        allitems[i].setAttribute("visibility", "false")
+    }
+   for (let i=0; i<lunches.length;i++){
+        lunches[i].setAttribute("visibility", "true")
+    }
 }
 
-alllunch.addEventListener("click",showLunch)
+ alllunch.addEventListener("click",showLunch)
 
 const showShakes = ()=>{
-    breakfast3.className="none";
-    shake3.className="meal";
-    breakfast1.className="none";
-    shake1.className="meal";
-    breakfast2.className="none";
-    shake2.className="meal";
-    dinner.className="none";
-    lunch1.className="none";
-    lunch2.className="none";
-    lunch3.className="none";
+    for (let i=0; i<allitems.length;i++){
+        allitems[i].setAttribute("visibility", "false")
+    }
+   for (let i=0; i<shakes.length;i++){
+        shakes[i].setAttribute("visibility", "true")
+    }
 }
 
 allshakes.addEventListener("click",showShakes)
 
 const showDinner = ()=>{
-    breakfast3.className="none";
-    shake3.className="none";
-    breakfast1.className="none";
-    shake1.className="none";
-    breakfast2.className="none";
-    shake2.className="none";
-    dinner.className="meal";
-    lunch1.className="none";
-    lunch2.className="none";
-    lunch3.className="none";
+    for (let i=0; i<allitems.length;i++){
+        allitems[i].setAttribute("visibility", "false")
+    }
+   for (let i=0; i<dinners.length;i++){
+        dinners[i].setAttribute("visibility", "true")
+    }
 }
 alldinner.addEventListener("click",showDinner)
